08cbfefac0985cc56c9a5f26a1e3dd2d8caf01ff
JIHEEYOU <yujihui5@gmail.com>
2025-11-23 07:20:50 +0900

    IP수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://18.188.164.109:8000',
         changeOrigin: true,
       },
     },
